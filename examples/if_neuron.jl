9ea9db4b3100ab92a2c117cdc0aa924b7ef57029
DrWatson <no@mail>
2023-04-04 12:35:59 +0200

    Examples files moved to DrWatson folder

---
+++ examples/if_neuron.jl
@@ -0,0 +1,13 @@
+using DrWatson
+@quickactivate "SNNExamples"
+
+using Plots
+using SpikingNeuralNetworks
+SNN.@load_units
+
+E = SNN.IF(;N = 1)
+E.I = [11]
+SNN.monitor(E, [:v, :fire])
+
+SNN.sim!([E], []; duration = 300ms)
+SNN.vecplot(E, :v)
